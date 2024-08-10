d8fb682f265963cfce352642838848624d42b300
nvnhan <nvnhan.dev@gmail.com>
2024-08-10 13:00:55 +0700

    update url

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://af6d6c00b098d4bc1998f47d76952763-1635652704.us-west-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a20d8e34faa7247db8d001002abc1fc1-187123522.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
